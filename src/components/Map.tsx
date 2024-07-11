9df93ca761a3dd36d51976cf062d1629fe8bf1ba
devanshhooda <devanshstan17@gmail.com>
2024-07-12 00:23:44 +0530

    Updated height of map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -15,7 +15,7 @@ const Map = ({ gps }: MapProps) => {
     const center = [gps[0], gps[1]];
 
     return (
-        <div className="w-60 h-60">
+        <div className="w-72 h-72">
             <MapContainer className="w-full h-full">
                 <SetViewOnLoad center={center} />
                 <TileLayer
